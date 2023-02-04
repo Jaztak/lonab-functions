7ef8f5db2cd498efa6d4a44d4caa75a7a820fc18
Jaztak <66039388+Jaztak@users.noreply.github.com>
2023-02-04 13:03:42 -0800

    Create txt-edit.js

---
+++ txt-edit.js
@@ -0,0 +1,8 @@
+
+if (document.body.contentEditable) {
+      document.body.contentEditable = false;
+      document.designMode = 'off';
+} else {
+      document.body.contentEditable = true;
+      document.designMode = 'on';
+}
