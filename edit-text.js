1d4c1f95f4c3ca2c0107d3c950804bacdc57bffd
Jaztak <66039388+Jaztak@users.noreply.github.com>
2023-02-04 13:03:51 -0800

    Delete edit-text.js

--- edit-text.js
+++
@@ -1,7 +0,0 @@
-if (document.body.contentEditable) {
-      document.body.contentEditable = false;
-      document.designMode = 'off';
-} else {
-      document.body.contentEditable = true;
-      document.designMode = 'on';
-}
