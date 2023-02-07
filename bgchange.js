d1ab0b905965e5bcaadcd9b9a55d55f87fdcdc9a
Jaztak <66039388+Jaztak@users.noreply.github.com>
2023-02-06 16:23:56 -0800

    Update bgchange.js

--- bgchange.js
+++ bgchange.js
@@ -1,24 +1,27 @@
-function inputRecolor() {
-        var inputs = [0, 1, 2, 3, 4];
-        var x = 0;
-        while (x < inputs.length) {
-          try {
+function invertColor(hex) {
+    if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+    }
+    // convert 3-digit hex to 6-digits.
+    if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+    if (hex.length !== 6) {
+      throw new Error('Invalid HEX color.');
+    }
+    // invert color components
+    var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+      g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+      b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+    // pad each with zeros and return
+    return '#' + padZero(r) + padZero(g) + padZero(b);
+  }
 
-            let val = document.getElementById("i" + x).value;
-            var color = "#000000";
-            if (val.includes("#")) {
-              var color = val;
-            } else {
-              var color = colourNameToHex(val.replace(" ", ""));
-            }
-            $("#" + x).css('background-color', "rgba(" + hexToRgb(color).r + ", " + hexToRgb(color).g + ", " + hexToRgb(color).b + ", 0.15)");
-            colors[x] = "rgba(" + hexToRgb(color).r + ", " + hexToRgb(color).g + ", " + hexToRgb(color).b + ", 0.9)"
-          } catch (err) {
-            $("#" + x).css('background-color', "rgba(255, 255, 255, 0.03)");
-          }
-          x = x + 1;
-        }
-      }
+  function padZero(str, len) {
+    len = len || 2;
+    var zeros = new Array(len).join('0');
+    return (zeros + str).slice(-len);
+  }
 function colourNameToHex(colour) {
         var colours = {
           "aliceblue": "#f0f8ff",
